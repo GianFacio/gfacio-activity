c2e20331bfc2ac2379ea6921396080fa367f87f8
Gian Facio <77515566+GianFacio@users.noreply.github.com>
2023-05-04 13:43:50 -0700

    Delete eclipse-workspace/CarManager directory

--- eclipse-workspace/CarManager/src/main/java/springboot/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import springboot.entity.Car;
-
-public interface CarRepository extends JpaRepository<Car, Long>{
-
-}
